6f5f341aa3044f687ebd13c41639f33161f17654
Louis Hobson <louis-hobson@hotmail.co.uk>
2023-12-29 17:52:19 +0000

    Variable name changes

--- src/main/scala/util/Partitions.scala
+++ src/main/scala/util/Partitions.scala
@@ -7,26 +7,26 @@ object Partitions {
 
     def apply(in: UInt, maxNumPartitions: Int): Seq[UInt] = {
         /* Get the maximum value plus one of in */
-        val max_in = 1 << in.getWidth
+        val maxIn = 1 << in.getWidth
 
         /* Iterate over the numbers of partitions which are greater than half of maxNumPartitions */
-        val upper_results = (maxNumPartitions/2+1 to maxNumPartitions).map(num_partitions => 
+        val upper_results = (maxNumPartitions/2+1 to maxNumPartitions).map(numPartitions => 
             /* Detect which partition (in) is within using a priority encoder fed with
              * inequality comparisons over the partition boundaries.
              */
-            PriorityEncoder((1 until num_partitions).map(partition => 
-                in < ((max_in*partition)/num_partitions).toInt.U
+            PriorityEncoder((1 until numPartitions).map(partition => 
+                in < ((maxIn*partition)/numPartitions).toInt.U
             ).appended(true.B))
         )
 
         /* The remaining numbers of partitions are under half of another number of partitions.
          * This means that the remaining results are bit shifts of those in upper_results .
          */
-        val lower_results = (1 to maxNumPartitions/2).map(num_partitions => {
-            /* Find maximum k>0 such that (num_partitions * 2^k <= maxNumPartitions) */
-            val k = log2Floor(maxNumPartitions/num_partitions)
-            /* Now derive this result from the result for (num_partitions*2^k) partitions */
-            upper_results((num_partitions << k)-maxNumPartitions/2-1) >> k
+        val lower_results = (1 to maxNumPartitions/2).map(numPartitions => {
+            /* Find maximum k>0 such that (numPartitions * 2^k <= maxNumPartitions) */
+            val k = log2Floor(maxNumPartitions/numPartitions)
+            /* Now derive this result from the result for (numPartitions*2^k) partitions */
+            upper_results((numPartitions << k)-maxNumPartitions/2-1) >> k
         })
 
         /* Concatenate the results and return */
